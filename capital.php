720c5acfe0853f931b63e76115dad71b8bb16313
DylanSS98 <72262587+DylanSS98@users.noreply.github.com>
2020-10-22 16:09:00 +0200

    Rien ne marche

--- capital.php
+++ capital.php
@@ -2,44 +2,38 @@
 $pdo = new PDO('mysql:host=mysql;dbname=basedetest;host=127.0.0.1', 'root', "", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-if (!isset($_POST['region'])) // Si la variable est vide, on affiche le formulaire des pays.
-{
-    $reponse=$pdo->query('SELECT Pays FROM pays') or die(print_r($pdo->errorInfo())); // Récupération de toutes les régions ?>
-    <form action="" method="post">
-        <p>
-            <label for="pays">Choisir un pays</label><br />
-            <select name="pays" id="pays" onchange="this.form.submit()">
-                <?php while ($donnees=$reponse->fetch())
-                {
-                    ?>
-                    <option value="<?php $donnees["Pays"]; ?>"><?php echo $donnees["Pays"]; ?></option>
-                    <?php
-                }
-                $reponse->closecursor(); // termine le traitement de la requete
-                ?>
-            </select>
-        </p>
-        <button type="submit" class="btn btn-secondary mt-5">Valider</button>
-    </form>
+$requete = $pdo->query('SELECT * FROM pays');
+$requete->bindParam('pays',$pays);
+$requete->bindParam('Capitale', $Capitale);
+$requete->execute();
+$pdo = $requete->fetchAll();{
+
 }
-<?php
-
-
-    $req=$pdo->prepare('SELECT Pays, Capitale FROM pays WHERE region = ?') or die(print_r($bdd->errorInfo()));
-    $req->execute(array($_POST['Capitale']));
-    $villes = $pdo->fetchAll();
-
-    if ()
 
 ?>
 
 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="utf-8" />
+    <title>Capitale</title>
+</head>
 
-
-
-
-
-
+<body>
+<h1>Capitale</h1>
+<form action="capital.php" method="GET">
+    <select name="Capitale" class="form-control mx-10">
+        <option>Default select</option>
+        <?php foreach ($Capitale as $items): ?>
+            <option name="<?= $items["Capitale"]?>" ><?= $items["Capitale"]?></option>
+        <?php endforeach;?>
+    </select>
+    <button type="submit" class="btn btn-secondary mt-5">Valider</button>
+</form>
+</body>
+</html>
 
 
 
