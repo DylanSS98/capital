be6ab7f1bf08a6752ae1dae705f43697126dd3a2
DylanSS98 <72262587+DylanSS98@users.noreply.github.com>
2020-10-22 11:12:53 +0200

    liste_deroulante_via_bdd

--- capital.php
+++ capital.php
@@ -2,45 +2,41 @@
 $pdo = new PDO('mysql:host=mysql;dbname=basedetest;host=127.0.0.1', 'root', "", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query("SELECT * FROM pays");
-
-$ville = $query->fetchAll();
-
-$capitale = $_REQUEST['Capitale'];
-
-if ($capitale === 'Paris'){
-    echo "C'est la France";
+if (!isset($_POST['region'])) // Si la variable est vide, on affiche le formulaire des pays.
+{
+    $reponse=$pdo->query('SELECT Pays FROM pays') or die(print_r($pdo->errorInfo())); // Récupération de toutes les régions ?>
+    <form action="" method="post">
+        <p>
+            <label for="pays">Choisir un pays</label><br />
+            <select name="pays" id="pays" onchange="this.form.submit()">
+                <?php while ($donnees=$reponse->fetch())
+                {
+                    ?>
+                    <option value="<?php $donnees["Pays"]; ?>"><?php echo $donnees["Pays"]; ?></option>
+                    <?php
+                }
+                $reponse->closecursor(); // termine le traitement de la requete
+                ?>
+            </select>
+        </p>
+        <button type="submit" class="btn btn-secondary mt-5">Valider</button>
+    </form>
 }
-elseif ($capitale === 'Berlin'){
-    echo "C'est l'allemagne";
-}
-elseif ($capitale === 'Lisbonne'){
-    echo "c'est le Portugal";
-}
+<?php
+
+
+    $req=$pdo->prepare('SELECT Pays, Capitale FROM pays WHERE region = ?') or die(print_r($bdd->errorInfo()));
+    $req->execute(array($_POST['Capitale']));
+    $villes = $pdo->fetchAll();
+
+    if ()
+
 ?>
 
 
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <meta charset="utf-8" />
-    <title>Capitale</title>
-</head>
 
-<body>
-<h1>Capitale</h1>
-<form action="capital.php" method="GET">
-<select name="Capitale" class="form-control mx-10">
-    <option>Default select</option>
-    <?php foreach ($ville as $items): ?>
-    <option name="<?= $items["Capitale"]?>" ><?= $items["Capitale"]?></option>
-    <?php endforeach;?>
-</select>
-    <button type="submit" class="btn btn-secondary mt-5">Valider</button>
-</form>
-</body>
-</html>
+
+
 
 
 
